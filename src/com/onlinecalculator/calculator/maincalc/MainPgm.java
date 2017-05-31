c03de7920287dfac9d339ff28e8fb834a3aaa4a3
HOME <HOME@192.168.0.8>
2017-05-31 18:30:32 +0530

    Calculator Main Program Code- Ch.Anusha

--- src/com/onlinecalculator/calculator/maincalc/MainPgm.java
+++ src/com/onlinecalculator/calculator/maincalc/MainPgm.java
@@ -6,26 +6,46 @@ import com.onlinecalculator.calculator.calctypes.*;
 
 public class MainPgm {
 public static void main(String args[]){
-        char type;
-        type=getType();
-        if(type=='a')
+        int x;
+        x=disp();
+		if(x==1)
         {
-        	
+			BasicCalc1 calc=new BasicCalc1();
+			calc.menu();
+			calc.userInputs();
+			calc.performOperation();
         }
-        if(type=='b')
+        if(x==2)
         {
-       		
+        	ScientificCalc scalc=new ScientificCalc();
+			scalc.menu();
+			scalc.userInputs();
+			scalc.performOperation();
        	}
         	
-        	if(type=='c')
+        	if(x==3)
         	{
-        		
+        		StatisticCalc scalc=new StatisticCalc();
+    			scalc.menu();
+    			scalc.userInputs();
+    			scalc.performOperation();
         	}
  
     }
         
-    static char getType()
-    {
-    	
-    }
+static int disp(){
+	int ch;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("**************** CALULATOR********************");
+	System.out.println("**************** 1.BASIC CALULATOR********************");
+	System.out.println("**************** 2.SCIENTIFIC CALULATOR********************");
+	System.out.println("**************** 3.STATISTIC CALULATOR********************");
+	System.out.println("**************** ********************");
+	System.out.println("Enter your Choice");
+	ch=sc.nextInt();
+	
+	return(ch);
+	
+}
+
 }
